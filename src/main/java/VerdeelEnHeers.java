bcc85ab205f50eb755bfc9c841a4480040b6504a
ArnoMolenaers <60972933+ArnoMolenaers@users.noreply.github.com>
2020-05-04 17:56:15 +0200

    backup

--- src/main/java/VerdeelEnHeers.java
+++ src/main/java/VerdeelEnHeers.java
@@ -1,16 +1,22 @@
 public class VerdeelEnHeers {
 
-
+    /**
+     * Dit programma zoekt het aantal volgorde fouten in een rij
+     * @author Arno Molenaers
+     * @param args Argumenten die worden mee gegeven aan het programma
+     * @since 21/04/2020
+     * @version 1.0
+     */
     public static void main(String[] args) {
         //voorbeelden vd les
-        //int[] rij = {1,4,9,5,8,6,7};
+        int[] rij = {1,4,9,5,8,6,7};
         //int[] rij = {1,4,6,9,5,7,8};
 
         //zelf
         //int[] rij = {1,6,3,5,4};                                /*  */
         //int[] rij = {1,4,3,5,8,6,7};                           /* 3 fouten */
         //int[] rij = {1,2,4,3,5,8,6,7,9,11,10,12,14,15};       /* 4 fouten */
-        int[] rij = {1,2,14,3,5,8,6,7,9,11,10,12,4,15};
+        //int[] rij = {1,2,14,3,5,8,6,7,9,11,10,12,4,15};
 
         System.out.println("amount: " + rij.length);
         System.out.print("rij: ");
@@ -18,14 +24,27 @@ public class VerdeelEnHeers {
             System.out.print(rij[i] + " , ");
         }
         System.out.println("");
-        int aantalfouten = GetaantalFouten(rij);
+        int aantalfouten = getAantalFouten(rij);
         System.out.println("\n \n ------------------------ FOUTEN: " + aantalfouten + " ------------------------");
     }
 
-    public static int GetaantalFouten(int[] rij){
+    /**
+     * @param rij de rij waar het aantal volgorde fouten van gevonden moet worden
+     * @return aantalvolgorde fouten in de rij
+     */
+    public static int getAantalFouten(int[] rij){
         return volgordeFouten(rij, 0 ,rij.length-1);
     }
 
+
+    /**
+     * recursieve functie die met de methode van verdeel en heers eerst de rij gaat op delen in deelrijen van grootte 2,
+     * dan gaat kijken of de volgorde klopt en deze oplossing gaat combineren met de andere deelrijen
+     * @param rij de rij waar het aantal volgorde fouten van gevonden moet worden
+     * @param begin het element waar de (deel)rij begint
+     * @param einde het element waar de (deel)rij eindigt
+     * @return aantalvolgorde fouten in de (deel)rij
+     */
     public static int volgordeFouten(int[] rij, int begin, int einde){
         int aantalFouten = 0;
 
